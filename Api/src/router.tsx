d91fbc2a548b20fe6fe10f1e72ad673480407e07
iamhyy123 <676854215@qq.com>
2025-06-28 20:35:02 +0800

    黄燕瑜添加代码

--- Api/src/router.tsx
+++ Api/src/router.tsx
@@ -2,8 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import BookCatalog from './pages/BookCatalog';
-import BorrowRecords from './pages/BorrowRecord';
+import BorrowRecords from './pages/BorrowRecords';
 import Login from './pages/Login';
+import ReaderManagement from './pages/ReaderManagement';
 import { AuthGuard } from './components/AuthGuard';
 
 export const router = createBrowserRouter([
@@ -24,7 +25,10 @@ export const router = createBrowserRouter([
         path: '/records',
         element: <AuthGuard><BorrowRecords /></AuthGuard>,
       },
-      
+      {
+        path: '/readers',
+        element: <AuthGuard><ReaderManagement /></AuthGuard>,
+      },
     ],
   },
 ]);
