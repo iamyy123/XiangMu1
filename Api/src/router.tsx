ca110d43ddac14d953f72e832cc4e7894a0606d6
LUOHONGYU0 <2188802031@qq.com>
2025-06-28 20:49:28 +0800

    罗鸿予

--- Api/src/router.tsx
+++ Api/src/router.tsx
@@ -2,9 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import BookCatalog from './pages/BookCatalog';
-import BorrowRecords from './pages/BorrowRecords';
+import BorrowRecords from './pages/BorrowReaders';
 import Login from './pages/Login';
-import ReaderManagement from './pages/ReaderManagement';
 import { AuthGuard } from './components/AuthGuard';
 
 export const router = createBrowserRouter([
@@ -25,10 +24,7 @@ export const router = createBrowserRouter([
         path: '/records',
         element: <AuthGuard><BorrowRecords /></AuthGuard>,
       },
-      {
-        path: '/readers',
-        element: <AuthGuard><ReaderManagement /></AuthGuard>,
-      },
+    
     ],
   },
 ]);
